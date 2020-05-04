2154d3dc14b83f03768ae638446deb81da66a0e9
ndgndg91 <ndgndg91@gmail.com>
2020-05-04 15:16:40 +0900

    <fix> mistake to service category skeleton

--- product/src/main/java/com/ndgndg91/commerce/product/controller/category/CategoryController.java
+++ product/src/main/java/com/ndgndg91/commerce/product/controller/category/CategoryController.java
@@ -1,12 +1,46 @@
 package com.ndgndg91.commerce.product.controller.category;
 
+import com.ndgndg91.commerce.product.common.page.Pageable;
+import com.ndgndg91.commerce.product.security.domain.JWTAuthentication;
 import com.ndgndg91.commerce.product.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Object> createCategory(
+            @AuthenticationPrincipal JWTAuthentication jwtAuthentication
+    ){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Object> readCategory(
+            @AuthenticationPrincipal JWTAuthentication jwtAuthentication,
+            @PathVariable(required = false) Long categoryId,
+            Pageable pageable){
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> updateCategory(
+            @AuthenticationPrincipal JWTAuthentication jwtAuthentication
+    ){
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> deleteCategory(
+            @AuthenticationPrincipal JWTAuthentication jwtAuthentication
+    ) {
+        return ResponseEntity.ok().build();
+    }
 }
--- product/src/main/java/com/ndgndg91/commerce/product/service/category/CategoryService.java
+++ product/src/main/java/com/ndgndg91/commerce/product/service/category/CategoryService.java
@@ -1,47 +1,13 @@
 package com.ndgndg91.commerce.product.service.category;
 
-import com.ndgndg91.commerce.product.common.page.Pageable;
 import com.ndgndg91.commerce.product.repository.category.CategoryRepository;
-import com.ndgndg91.commerce.product.security.domain.JWTAuthentication;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
 @Service
 @RequiredArgsConstructor
-@RequestMapping("/categories")
 public class  CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    @PostMapping
-    public ResponseEntity<Object> createCategory(
-            @AuthenticationPrincipal JWTAuthentication jwtAuthentication
-    ){
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/{categoryId}")
-    public ResponseEntity<Object> readCategory(
-            @AuthenticationPrincipal JWTAuthentication jwtAuthentication,
-            @PathVariable(required = false) Long categoryId,
-            Pageable pageable){
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping
-    public ResponseEntity<Object> updateCategory(
-            @AuthenticationPrincipal JWTAuthentication jwtAuthentication
-    ){
-        return ResponseEntity.ok().build();
-    }
-
-    @DeleteMapping
-    public ResponseEntity<Object> deleteCategory(
-            @AuthenticationPrincipal JWTAuthentication jwtAuthentication
-    ) {
-        return ResponseEntity.ok().build();
-    }
 }
